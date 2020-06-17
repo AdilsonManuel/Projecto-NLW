91061af8c4e67db015a24123b34a1c7707329ffd
adilsonZM <adilsonzumba@hotmail.com>
2020-06-17 23:21:00 +0100

    instalação do Yarn para auxiliar na instalação das dependencias,e instalar o nodemon

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-const express = require
+const express = require("express")
+const server = express()
+
+//Configurar caminhos da minha aplicação
+//Página inicial
+//res-> response - req->request
+server.get("/", (req,res) =>{
+    res.send("teste")
+})
+
+//Ligar o servidor
+server.listen(1303)
