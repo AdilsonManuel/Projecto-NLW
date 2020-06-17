c677e03fc13c99d39e2eab851b13f4c7a2377727
adilsonZM <adilsonzumba@hotmail.com>
2020-06-18 00:10:16 +0100

    Instalação e configuração do nunjucks

--- src/server.js
+++ src/server.js
@@ -1,11 +1,26 @@
 const express = require("express")
 const server = express()
 
+//Configura pasta publica
+server.use(express.static("public"))
+
+
+//Utilizando template engine
+const nunjucks = require("nunjucks")
+
 //Configurar caminhos da minha aplicação
 //Página inicial
 //res-> response - req->request
 server.get("/", (req,res) =>{
-    res.send("teste")
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req,res) =>{
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+server.get("/search.results", (req,res) =>{
+    res.sendFile(__dirname + "/views/search.results.html")
 })
 
 //Ligar o servidor
